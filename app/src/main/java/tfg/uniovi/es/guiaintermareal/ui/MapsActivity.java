df6c795449f50c337a76591ec5dd3efa391d75a5
Miguel Angel <miguelangel.feijoo@gmail.com>
2017-05-11 03:55:38 +0200

    Obtener localizacion actual. Creacion del LocationProvider y modificacion de MapsActivity y permisos del Manifest

--- app/src/main/java/tfg/uniovi/es/guiaintermareal/ui/MapsActivity.java
+++ app/src/main/java/tfg/uniovi/es/guiaintermareal/ui/MapsActivity.java
@@ -1,7 +1,9 @@
 package tfg.uniovi.es.guiaintermareal.ui;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,31 +13,87 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import tfg.uniovi.es.guiaintermareal.R;
+import tfg.uniovi.es.guiaintermareal.provider.LocationProvider;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements LocationProvider.LocationCallback, OnMapReadyCallback {
+
+    public static final String TAG = MapsActivity.class.getSimpleName();
+
+    private GoogleMap mMap;
+    private LocationProvider mLocationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
-        // Get the SupportMapFragment and request notification
-        // when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setUpMapIfNeeded();
+
+        mLocationProvider = new LocationProvider(this, this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // Add a marker in Gijon, España
-        // and move the map's camera to the same location.
-        LatLng m = new LatLng(43.541464, -5.650547);
-        googleMap.addMarker(new MarkerOptions().position(m)
-                .title("Playa de San Lorenzo"));
-        LatLng m2 = new LatLng(43.543036, -5.670914);
-        googleMap.addMarker(new MarkerOptions().position(m2)
-                .title("Playa de Poniente"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(m));
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mLocationProvider.connect();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+
+    public void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(currentLatitude, currentLongitude)).title("Posicion actual"));
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("Estoy aqui!");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        setUpMap();
+    }
+
+
+    public void setUpMap(){
+        try {
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            mMap.setMyLocationEnabled(true);
+            mMap.setTrafficEnabled(true);
+            mMap.setIndoorEnabled(true);
+            mMap.setBuildingsEnabled(true);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+        }catch(SecurityException e){
+            Log.d("setUpMat", "Error al configurar el mapa");
+        }
+    }
+
 }
